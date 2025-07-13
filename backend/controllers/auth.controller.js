76e53319308b2f80e8ecb882f399b184da700b1a
LWardDev <lwarddev@gmail.com>
2025-07-13 22:38:00 +0100

    Added signup api functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,63 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+import { generateTokenAndSetCookie } from '../lib/utils/generateToken.js';
+
 export const signup = async (req, res) => {
-  res.json({ data: 'You hit the signup endpoint' });
+  try {
+    const { fullName, username, email, password } = req.body;
+
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: 'Invalid email format' });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: 'Password must be more than 6 characters' });
+    }
+
+    //Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        _id: newUser._id,
+        username: newUser.username,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        follower: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: 'Invalid user data' });
+    }
+  } catch (error) {
+    console.log(`Error in signup function: ${error.message}`);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
@@ -7,10 +7,13 @@ import connectMongoDB from './db/connectMongoDb.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(8000, () => {
   console.log(`Server is running on port: ${PORT}`);
   connectMongoDB();
