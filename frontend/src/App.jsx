81ce6b488e6b3b17f12e8ee2aa1130044ddf1e18
LWardDev <lwarddev@gmail.com>
2025-07-26 21:58:43 +0100

    ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,15 @@ import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import HomePage from "./pages/home/HomePage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route
           path="/"
@@ -19,7 +24,10 @@ function App() {
           path="/signup"
           element={<SignUpPage />}
         />
+        <Route path="/profile/:username" element={<ProfilePage />}/>
+        <Route path="/notifications" element={<NotificationPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
